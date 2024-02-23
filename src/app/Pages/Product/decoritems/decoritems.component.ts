3e4fce077266f730cc85215c5498701834f9ffec
Harsh Fotariya <154208288+HarshFotariya10@users.noreply.github.com>
2024-02-24 01:26:40 +0530

    Add Categories Api

--- src/app/Pages/Product/decoritems/decoritems.component.ts
+++ src/app/Pages/Product/decoritems/decoritems.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./decoritems.component.css']
 })
 export class DecoritemsComponent {
+  constructor(private http :HttpClient){}
+  ngOnInit(): void {
+    this.getFurnitureCategory();
+  }
 
+  decorItemCategory :any[] = [];
+    getFurnitureCategory(){
+      this.http.get('http://localhost:8080/categories/decoritems').subscribe((data:any)=>{ 
+        this.decorItemCategory=data;
+        console.log(this.decorItemCategory)
+      })
+    }
 }
--- src/app/Pages/Product/furniture/furniture.component.ts
+++ src/app/Pages/Product/furniture/furniture.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-furniture',
   templateUrl: './furniture.component.html',
   styleUrls: ['./furniture.component.css']
 })
-export class FurnitureComponent {
+export class FurnitureComponent implements OnInit{
 
+  constructor(private http :HttpClient){}
+  ngOnInit(): void {
+    this.getFurnitureCategory();
+  }
+
+  furnitureCategory :any[] = [];
+    getFurnitureCategory(){
+      this.http.get('http://localhost:8080/categories/furniture').subscribe((data:any)=>{ 
+        this.furnitureCategory=data;
+        console.log(this.furnitureCategory)
+      })
+    }
 }
